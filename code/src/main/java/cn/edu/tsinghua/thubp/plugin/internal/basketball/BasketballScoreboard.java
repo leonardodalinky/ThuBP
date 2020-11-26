09a96441882755a62d3d2954024c9c001daa11ac
Rhacoal <rhacoal@gmail.com>
2020-11-26 15:09:54 +0800

    添加按照类型列出赛事 API

--- code/src/main/java/cn/edu/tsinghua/thubp/plugin/internal/basketball/BasketballScoreboard.java
+++ code/src/main/java/cn/edu/tsinghua/thubp/plugin/internal/basketball/BasketballScoreboard.java
@@ -1,6 +1,8 @@
 package cn.edu.tsinghua.thubp.plugin.internal.basketball;
 
 import cn.edu.tsinghua.thubp.plugin.api.scoreboard.GameScoreboard;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @GameScoreboard.GameScoreboardInfo(scoreboardTypeId = "default", scoreboardTypeName = "标准比赛")
 public class BasketballScoreboard implements GameScoreboard<BasketballScoreboardConfig, BasketballGameResult> {
@@ -9,8 +11,18 @@ public class BasketballScoreboard implements GameScoreboard<BasketballScoreboard
         return null;
     }
 
+    @Getter
+    @AllArgsConstructor
+    public enum BasketballValidationResult implements ValidationResult {
+        OK(true, 0, "ok"),
+        ;
+        private final boolean valid;
+        private final int code;
+        private final String message;
+    }
+
     @Override
     public ValidationResult isValid(BasketballScoreboardConfig config, BasketballGameResult basketballGameResult) {
-        return null;
+        return BasketballValidationResult.OK;
     }
 }
